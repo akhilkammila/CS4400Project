e4b41b6340284bef0bf7bc3ca5ddd7083feae05a
Akhil Kammila <akhilkammila@gmail.com>
2023-03-28 17:07:44 -0400

    initial commit with airline create table file

---
+++ Airline.sql
@@ -0,0 +1,25 @@
+-- CS4400: Introduction to Database Systems: Monday, January 30, 2023
+-- Flight Management Course Project Database TEMPLATE (v1.0)
+/* This is a standard preamble for most of our scripts. The intent is to establish
+a consistent environment for the database behavior. */
+set global transaction isolation level serializable;
+set global SQL_MODE = 'ANSI,TRADITIONAL';
+set names utf8mb4;
+set SQL_SAFE_UPDATES = 0;
+set @thisDatabase = 'flight_management';
+drop database if exists flight_management;
+create database if not exists flight_management;
+use flight_management;
+-- Define the database structures and enter the denormalized data
+
+
+-- 1. Airlines
+DROP TABLE IF EXISTS airline;
+CREATE TABLE airline(
+	airlineID varchar(50) not null,
+    revenue int not null,
+    PRIMARY KEY(airlineID)
+) ENGINE=innodb;
+
+INSERT INTO airline VALUES
+('Air_France', '25')
