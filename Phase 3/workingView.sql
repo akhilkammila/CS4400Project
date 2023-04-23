61c00cf42cbb6e3696c9b58ab4d7a80675e18868
Akhil Kammila <akhilkammila@gmail.com>
2023-04-23 17:11:11 -0400

    fixed q15

--- Phase 3/workingView.sql
+++ Phase 3/workingView.sql
@@ -1,27 +1,6 @@
-SELECT
-    l.departure AS departing_from,
-    l.arrival AS arriving_at,
-    COUNT(DISTINCT a.tail_num) AS num_airplanes,
-    GROUP_CONCAT(DISTINCT a.locationID) AS airplane_list,
-    GROUP_CONCAT(DISTINCT f.flightID) AS flight_list,
-    MIN(f.next_time) AS earliest_arrival,
-    MAX(f.next_time) AS latest_arrival,
-    COUNT(DISTINCT pi.personID) AS num_pilots,
-    COUNT(DISTINCT pa.personID) AS num_passengers,
-    COUNT(DISTINCT p.personID) AS joint_pilots_passengers,
-    GROUP_CONCAT(DISTINCT p.personID) AS person_list
+SELECT MAX(rp.sequence)
 FROM flight as f
-JOIN airplane as a
-ON f.support_airline = a.airlineID AND f.support_tail = a.tail_num
-JOIN person as p
-ON a.locationID = p.locationID
 JOIN route_path as rp
-ON f.routeID = rp.routeID and progress = sequence
-JOIN leg as l
-ON rp.legID = l.legID
-left JOIN pilot as pi
-ON p.personID = pi.personID
-left JOIN passenger as pa
-ON p.personID = pa.personID
-WHERE airplane_status = 'in_flight'
-GROUP BY l.departure, l.arrival;
+ON f.routeID = rp.routeID
+WHERE f.flightID = 'AM_1523'
+GROUP BY(f.routeID);
