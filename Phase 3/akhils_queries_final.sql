1e22e7644c642a53b890292919b5ccc5e1a770cc
Akhil Kammila <akhilkammila@gmail.com>
2023-04-23 13:48:19 -0400

    q21 done

--- Phase 3/akhils_queries_final.sql
+++ Phase 3/akhils_queries_final.sql
@@ -473,7 +473,33 @@ select null, 0, null, null, null, null;
 create or replace view people_in_the_air (departing_from, arriving_at, num_airplanes,
 	airplane_list, flight_list, earliest_arrival, latest_arrival, num_pilots,
 	num_passengers, joint_pilots_passengers, person_list) as
-select null, null, 0, null, null, null, null, 0, 0, null, null;
+SELECT
+    l.departure AS departing_from,
+    l.arrival AS arriving_at,
+    COUNT(DISTINCT a.tail_num) AS num_airplanes,
+    GROUP_CONCAT(DISTINCT a.locationID) AS airplane_list,
+    GROUP_CONCAT(DISTINCT f.flightID) AS flight_list,
+    MIN(f.next_time) AS earliest_arrival,
+    MAX(f.next_time) AS latest_arrival,
+    COUNT(DISTINCT pi.personID) AS num_pilots,
+    COUNT(DISTINCT pa.personID) AS num_passengers,
+    COUNT(DISTINCT p.personID) AS joint_pilots_passengers,
+    GROUP_CONCAT(DISTINCT p.personID) AS person_list
+FROM flight as f
+JOIN airplane as a
+ON f.support_airline = a.airlineID AND f.support_tail = a.tail_num
+JOIN person as p
+ON a.locationID = p.locationID
+JOIN route_path as rp
+ON f.routeID = rp.routeID and progress = sequence
+JOIN leg as l
+ON rp.legID = l.legID
+left JOIN pilot as pi
+ON p.personID = pi.personID
+left JOIN passenger as pa
+ON p.personID = pa.personID
+WHERE airplane_status = 'in_flight'
+GROUP BY l.departure, l.arrival;
 
 -- [22] people_on_the_ground()
 -- -----------------------------------------------------------------------------
